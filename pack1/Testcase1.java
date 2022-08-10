e4a5738425d2a39de4b049e59db8830358a02eb4
udayk <udayk@DESKTOP-UJFK23I.bbrouter>
2022-08-10 09:55:30 +0530

    Test case update with @Test and locator change.

--- pack1/Testcase1.java
+++ pack1/Testcase1.java
@@ -1,13 +1,22 @@
 package pack1;
 
+import org.testng.annotations.Test;
+
 public class Testcase1 {
 
 	// Test case for step1
 	//Test case added step#2
 	
-	public static void main(String[] args) {
+	@Test
+	public void testcase1() {
+		//launch browser.
+		//startBrowser("chrome");
+		
 		System.out.println("Hello World");
 		//Test case update - xpath
+		//locator update.
+		//close browser.
+		
 	}
 	
 }
